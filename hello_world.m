d03a142b1d8415b157b8b6b359ef9a19af91f194
Jesus Jimenez <jedjimenezsav1@gmail.com>
2024-07-03 20:04:28 +0200

    foregroung to 3d

--- hello_world.m
+++
@@ -1,3 +0,0 @@
-a = 1 + 2
-str = "Hello World jesus"
-str2 = "new bransh test 1"
--- jesus_main/integrationAttemptMain_jesus.m
+++ jesus_main/integrationAttemptMain_jesus.m
@@ -1,12 +1,75 @@
-% load a image
-% this code add padding to the image after user select the rectangle
-clc
-clear all
-close all
-image = imread('/Users/jesusjimenez/Documents/MyGitProjects/Computer-Vision-Final-Project_sose2024/jesus_main/CV-Challenge-24-Datensatz/oil-painting.png');
-size(image)
-%rgb = [double(image(100,100,1))/255, double(image(100,100,2))/255, double(image(100,100,3))/255];
-%rgb
+
+% Let the user select the image file
+[filename, pathname] = uigetfile({'*.png;*.jpg;*.jpeg;*.bmp', 'Image Files (*.png, *.jpg, *.jpeg, *.bmp)'}, 'Select an Image');
+if isequal(filename, 0)
+    disp('User selected Cancel');
+    return;
+else
+    imagePath = fullfile(pathname, filename);
+end
+
+% Load the image
+image = imread(imagePath);
+
+% Display the image
+figure;
+imshow(image);
+title('Original Image');
+% Ask the user for the number of foreground objects
+%numObjects = input('Enter the number of foreground objects: ');
+numObjects = 1;
+
+
+% Initialize the combined mask
+combinedMask = false(size(image, 1), size(image, 2));
+
+% Loop to manually select each foreground object
+for i = 1:numObjects
+    figure;
+    imshow(image);
+    title(['Select foreground object ' num2str(i)]);
+    h = drawpolygon();
+    mask = createMask(h);
+    
+    % Combine the masks
+    combinedMask = combinedMask | mask;
+end
+
+
+% Save the combined mask
+imwrite(combinedMask, 'combined_mask.png');
+
+% Extract the foreground objects using the combined mask
+foreground = bsxfun(@times, image, cast(combinedMask, 'like', image));
+
+% Save the extracted foreground objects
+imwrite(foreground, 'extracted_foreground.png');
+
+% Display the extracted foreground
+
+% Create the background by removing the foreground objects
+background = image;
+background(repmat(combinedMask, [1, 1, 3])) = 0;
+
+
+
+% Fill the holes in the background using inpaintExemplar
+backgroundDouble = im2double(background);
+filledBackground = inpaintExemplar(backgroundDouble, combinedMask);
+
+% Save the filled background
+imwrite(filledBackground, 'filled_background.png');
+
+% Display the filled background
+figure;
+imshow(filledBackground);
+title('Filled Background');
+
+
+
+% Load the filled background image
+image = imread('filled_background.png');
+
 f = 300;
 % #########################################################
 % STEP 1 select points on image and get vertices
@@ -23,13 +86,12 @@ f = 300;
 %  point 11;
 %  point 12;
 %  vanish point
-px_vertices2d = select_points(image)
-%{
-% Estimate focal length
-f=focal_length(px_vertices2d)
+px_vertices2d = select_points(image);
 
+% focal length estimation
+f=focal_length(px_vertices2d)
 % #########################################################
-% STEP2  calcule Vertices in 3D using camera focal length
+% calcule Vertices in 3D using camera focal length
 [px_h, px_w, c] = size(image);
 % invert "y" pixel s
 vertices2d(:, 1) = px_vertices2d(:, 1);
@@ -37,10 +99,10 @@ vertices2d(:, 2) = (px_h - px_vertices2d(:, 2) + 1);
 
 vertices3d = vertices3D(vertices2d, f);
 
-
 % #########################################################
-% STEP 3 save all pixel coordinate into a new matrix (px_x, px_y, rgb values)
+% save all pixel coordinate into a new matrix (px_x, px_y, rgb values)
 px_coord2d = zeros(px_h*px_w, 5);
+
 
 
 for i=1:px_h
@@ -51,33 +113,54 @@ for i=1:px_h
     end
 end
 
+% save all foregroung pixel in a matirx (px_x, px_y, rgb values)
+foreground_size =  size(combinedMask(combinedMask(:,:) == 1), 1)
+size(image)
+size(foreground)
+px_foreground_coord2d = zeros(foreground_size, 5);
 
+itemp = 0;
+
+for i=1:px_h
+    for j=1:px_w
+        if combinedMask(i,j)== 1 
+            itemp = itemp+1;
+           
+            px_foreground_coord2d(itemp,1)=j;
+            px_foreground_coord2d(itemp,2)=i;
+            px_foreground_coord2d(itemp,3:5)=foreground(i,j,:);
+        end
+    end
+end
+
+itemp = itemp
 % invert "y" pixel 
 px_coord2d(:, 2) = px_h - px_coord2d(:, 2) + 1;
+px_foreground_coord2d(:, 2) = px_h - px_foreground_coord2d(:, 2) + 1;
 % scale coordinates
 %px_coord2d(:, 1) =  px_coord2d(:, 1) / px_w;
 %px_coord2d(:, 2) =  px_coord2d(:, 2) / px_h;
-
-% ##########################################################
-% Convert from to 2d to 3d
 
 height  = vertices3d(7,2);
 leftx   = vertices3d(1,1);
 rightx  = vertices3d(4,1);
 
-point_cloud = image2dto3d(px_coord2d, vertices2d,vertices3d,px_h,px_w,f,height,leftx,rightx);
+coord3d = image2dto3d(px_coord2d, vertices2d,vertices3d,f,height,leftx,rightx, px_foreground_coord2d);
 
-x = point_cloud(:,1);
-y = point_cloud(:,2);
-z = point_cloud(:,3);
 
-rgb = point_cloud(:,4:6)/255;
+xx=coord3d(:,1);
+yy=coord3d(:,2);
+zz=coord3d(:,3);
+max(coord3d(:,1))
+max(coord3d(:,2))
+max(abs(coord3d(:,3)))
+vertices3d(1,3)
+color=coord3d(:,4:6)/255;
 
-pcshow([x y z], rgb, 'VerticalAxisDir', 'Down')
+pcshow([xx yy zz],color,'VerticalAxisDir','Down')
 %set(gcf,'color','[0.94,0.94,0.94]');
 %set(gca,'color','[0.94,0.94,0.94]');
 view([0, 0]);
-%}
 
 function f = focal_length(px_coord2d)
 
@@ -94,11 +177,20 @@ end
 
 function coords = select_points(image)
     % this function take as input a image 
-    %return [ vanish point; 
-    %         point 1; down left 
-    %         point 2: down right
-    %         point 7: up left
-    %         point 8] up right
+    % return
+    % [point 1;
+    %  point 2;
+    %  point 3;
+    %  point 4;
+    %  point 5;
+    %  point 6;
+    %  point 7;
+    %  point 8;
+    %  point 9;
+    %  point 10;
+    %  point 11;
+    %  point 12;
+    %  vanish point]
 
     % copy the image
     img = image;
@@ -165,7 +257,6 @@ function coords = select_points(image)
     vertices2D_px(12,:) = [x_max;                                 (x_max-x_vp)*gradient(3) + y_vp];
     vertices2D_px(9,:)  = [(1-y_vp)/gradient(4) + x_vp;           1];
     vertices2D_px(11,:) = [1;                                     (1-x_vp)*gradient(4) + y_vp];
-    
 
     hold on;
     for i = 1:12
@@ -174,40 +265,7 @@ function coords = select_points(image)
         text(vertices2D_px(i,1),vertices2D_px(i,2),num2str(i),'Color', 'green', 'FontSize', 30,'FontWeight', 'bold')
     end
     hold off;
-    % Define the padding size
-    [m,n,c] = size(img)
-    paddingLeft = ceil(min([abs(vertices2D_px(5,1)), abs(vertices2D_px(11,1)), abs(vertices2D_px(3,1)), abs(vertices2D_px(9,1))]))
-    paddingRight = ceil(max([abs(vertices2D_px(4,1)), abs(vertices2D_px(10,1)), abs(vertices2D_px(6,1)), abs(vertices2D_px(12,1))]))-n+1
-    paddingTop = ceil(max([abs(vertices2D_px(9,2)), abs(vertices2D_px(10,2)), abs(vertices2D_px(12,2)), abs(vertices2D_px(11,2))]))
-    paddingBottom = ceil(max([abs(vertices2D_px(3,2)), abs(vertices2D_px(4,2)), abs(vertices2D_px(6,2)), abs(vertices2D_px(5,2))]))-m+1
-
-    
-    % Define the padding color (RGB values between 0 and 255)
-    paddingColor = 0; % Red color
-
-    % Create a new padded image with the specified padding
-    paddedRows = m + paddingTop + paddingBottom;
-    paddedCols = n + paddingLeft + paddingRight;
-    paddedImg = uint8(zeros(paddedRows, paddedCols, c));
-
-    % Fill the new image with the padding color
-    paddedImg(:, :, 1) = paddingColor;
-    paddedImg(:, :, 2) = paddingColor;
-    paddedImg(:, :, 3) = paddingColor;
-
-    % Place the original image in the center of the padded image
-    paddedImg(paddingTop+1:paddingTop+m, paddingLeft+1:paddingLeft+n, :) = img;
-    figure;
-   
-    imshow(paddedImg);
-
-    hold on;
-    for i = 1:12
-        plot([vertices2D_px(i,1)+paddingLeft, x_vp+paddingLeft],[vertices2D_px(i,2)+paddingTop,y_vp+paddingTop],'r-','lineWidth',3)
-        plot(vertices2D_px(i,1)+paddingLeft, vertices2D_px(i,2)+paddingTop, '.', 'MarkerSize', 15, 'LineWidth', 5, 'Color', 'green'); % Point 2
-        text(vertices2D_px(i,1)+paddingLeft,vertices2D_px(i,2)+paddingTop,num2str(i),'Color', 'green', 'FontSize', 30,'FontWeight', 'bold')
-    end
-    hold off;
+  
     % Return coordinates
     coords = [vertices2D_px; vpoint];
 end
@@ -227,8 +285,8 @@ function vertices3d = vertices3D(vertices2d, f)
         vertices3d(i,2) = 0;
         
     end
-    vertices3d(13,1) = view_x; 
-    vertices3d(13,2) = view_y 
+    vertices3d(13,1) = view_x;
+    vertices3d(13,2) = view_y;
     vertices3d(13,3) = vertices3d(2,3);
 
     height=-(vertices2d(7,2)-vertices2d(1,2))* vertices3d(1,3)/f;
@@ -252,12 +310,11 @@ function vertices3d = vertices3D(vertices2d, f)
     
 end
 
-function [coord3d] = image2dto3d(coord2d,corners2d,corners3d,m,n,f,height,leftx,rightx)
+function [coord3d] = image2dto3d(coord2d,corners2d,corners3d,f,height,leftx,rightx, foreground_coord2d)
     % % input:
     % coord2d: 2d coordinates of all pixels  
     % corners2d: 2d coordinates of corners points 
     % corners3d: 3d coordinates of corners points 
-    % m,n: size of image
     % f:focal length 
     % height: ceil y coordinate 
     % leftx: left wall x coordinate
@@ -265,29 +322,29 @@ function [coord3d] = image2dto3d(coord2d,corners2d,corners3d,m,n,f,height,leftx,
     % output:
     % coord3: 3d coordinates of all pixels  
     % %
-    coord2d(:,1)=coord2d(:,1);
-    coord2d(:,2)=coord2d(:,2);
+    %coord2d(:,1)=coord2d(:,1);%;
+    %coord2d(:,2)=coord2d(:,2);%;
     
     
     vp=corners3d(13,:);
     t1=corners2d(1,:)-vp(1:2);
-    
     t2=corners2d(2,:)-vp(1:2);
     t7=corners2d(7,:)-vp(1:2);
     t8=corners2d(8,:)-vp(1:2);
-    is_bottom_plane= @(point2d) (point2d(2) <= t1(2)) && (point2d(2) <= point2d(1)*t1(2)/t1(1)) && (point2d(2) <= point2d(1)*t2(2)/t2(1));
-    is_right_plane= @(point2d) (point2d(1) >= t2(1)) && (point2d(2) <= point2d(1)*t8(2)/t8(1)) && (point2d(2)>= point2d(1)*t2(2)/t2(1));
-    is_top_plane= @(point2d) (point2d(2)>=t8(2)) && (point2d(2) >= point2d(1)*t8(2)/t8(1)) && (point2d(2)>=point2d(1) * t7(2)/t7(1));
-    is_left_plane=@(point2d) (point2d(1) <= t7(1)) && (point2d(2)<=point2d(1) *t7(2)/t7(1)) && (point2d(2)>=point2d(1)*t1(2)/t1(1) );
+    is_bottom__plane = @(point2d) (point2d(2) <= t1(2)) && (point2d(2) <= point2d(1)*t1(2)/t1(1)) && (point2d(2) <= point2d(1)*t2(2)/t2(1));
+    is_right_plane = @(point2d) (point2d(1) >= t2(1)) && (point2d(2) <= point2d(1)*t8(2)/t8(1)) && (point2d(2)>= point2d(1)*t2(2)/t2(1));
+    is_top_plane = @(point2d) (point2d(2)>=t8(2)) && (point2d(2) >= point2d(1)*t8(2)/t8(1)) && (point2d(2)>=point2d(1) * t7(2)/t7(1));
+    is_left_plane = @(point2d) (point2d(1) <= t7(1)) && (point2d(2)<=point2d(1) *t7(2)/t7(1)) && (point2d(2)>=point2d(1)*t1(2)/t1(1) );
     is_center_plane = @(point2d)  (point2d(1)<=t2(1)) && (point2d(1) >=t1(1)) && (point2d(2)<=t7(2)) && (point2d(2) >=t1(2));
     
     length=size(coord2d,1);
-    coord3d=zeros(length,6);
+    length_foreground=size(foreground_coord2d,1);
+    coord3d=zeros(length+length_foreground,6);
 
     for i=1:length
         point2d=coord2d(i,1:2);
         coord3d(i,4:6)=coord2d(i,3:5);
-        if is_bottom_plane(point2d-vp(1:2))
+        if is_bottom__plane(point2d-vp(1:2))
             coord3d(i,2)=0;
             coord3d(i,3)=-vp(2)/(vp(2)-point2d(2))*f;
             coord3d(i,1)=-coord3d(i,3)/f*(point2d(1)-vp(1))+vp(1);
@@ -304,12 +361,46 @@ function [coord3d] = image2dto3d(coord2d,corners2d,corners3d,m,n,f,height,leftx,
             coord3d(i,3)=-(vp(1)-rightx)/(vp(1)-point2d(1))*f;
             coord3d(i,2)=-coord3d(i,3)/f*(point2d(2)-vp(2))+vp(2);
         elseif is_center_plane(point2d-vp(1:2))
-            coord3d(i,3)=vp(3);
-            coord3d(i,2)=-coord3d(i,3)/f*(point2d(2)-vp(2))+vp(2);
-            coord3d(i,1)=-coord3d(i,3)/f*(point2d(1)-vp(1))+vp(1);
+            coord3d(i,3)= vp(3); 
+            coord3d(i,2)=-vp(3)/f*(point2d(2)-vp(2))+vp(2);
+            coord3d(i,1)=-vp(3)/f*(point2d(1)-vp(1))+vp(1); 
 
         
         end
     end
-end
     
+    fz_temp = 0;
+    %[fore_x_min, fore_x_min] = [min(foreground_coord2d(:,1)), max(foreground_coord2d(:,1))];
+    %[fore_y_min, fore_y_min] = [min(foreground_coord2d(:,1)), max(foreground_coord2d(:,1))];
+    point2d=foreground_coord2d(1,1:2);
+  
+    if is_bottom__plane(point2d-vp(1:2))
+        fz_temp=-vp(2)/(vp(2)-point2d(2))*f;
+    
+    elseif is_top_plane(point2d-vp(1:2))
+        fz_temp=-(vp(2)-height)/(vp(2)-point2d(2))*f;
+        
+    elseif is_left_plane(point2d-vp(1:2))
+        fz_temp=-(vp(1)-leftx)/(vp(1)-point2d(1))*f;
+    elseif is_right_plane(point2d-vp(1:2))
+        fz_temp= -(vp(1)-rightx)/(vp(1)-point2d(1))*f;
+    elseif is_center_plane(point2d-vp(1:2))
+        fz_temp=vp(3); 
+    end
+    % calculate foregound z
+    vp(3)
+    fz_temp =-1000
+    
+   
+  
+    z_for = fz_temp
+    for i = 1:length_foreground
+        point2d=foreground_coord2d(i,1:2);
+        coord3d(i+length,4:6)=foreground_coord2d(i,3:5);
+
+        coord3d(i+length,3)= z_for; 
+        coord3d(i+length,2)=-z_for/f*(point2d(2)-vp(2))+vp(2);
+        coord3d(i+length,1)=-z_for/f*(point2d(1)-vp(1))+vp(1); 
+    end
+end
+
