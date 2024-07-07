7c1807593dba73b3c264c4df9d8e4b8f3ff7806f
Jesus Jimenez <jedjimenezsav1@gmail.com>
2024-07-07 22:36:15 +0200

    more docu

--- main.m
+++ main.m
@@ -162,8 +162,9 @@ function new_img = create_new_img(pixels3d, vpx, vpy, f, R, T, h, w)
     vector2d=zeros(length,5);
     new_img=NaN(h,w,3);
     for i=1:length
-     
+        % Estimate X 2d coordinate
         vector2d(i,1)= round(-f/pixels3d_transformed(i,3)*(pixels3d_transformed(i,1)-vpx)+vpx);
+        % Estimate Y 2d coordinate and also invert so match for the new image
         vector2d(i,2)= -round(-f/pixels3d_transformed(i,3)*(pixels3d_transformed(i,2)-vpy)+vpy)+h+1;
         vector2d(i,3:5)=pixels3d(i,4:6);
         
