dda1689fbc66467c6d66c41be3202601a91688af
Jesus Jimenez <jedjimenezsav1@gmail.com>
2024-07-07 22:13:31 +0200

    just ohne object

--- main.m
+++ main.m
@@ -17,7 +17,7 @@ imshow(image);
 title('Original Image');
 % Ask the user for the number of foreground objects
 %numObjects = input('Enter the number of foreground objects: ');
-numObjects = input('Enter the number of foreground objects: ');
+numObjects = 1;
 
 
 % Initialize the combined mask
