ba645fb96ecd20797b8f9561ff1e58c70e21b17b
Jesus Jimenez <jedjimenezsav1@gmail.com>
2024-07-08 18:37:07 +0200

    delate extra variable

--- GUI/create_new_img.m
+++ GUI/create_new_img.m
@@ -7,22 +7,17 @@ function new_img = create_new_img(pixels3d, vpx, vpy, f, R, T, h, w)
     pixels3d_transformed=R*pixels3d_centered+T-offset';
     pixels3d_transformed = pixels3d_transformed';
 
-    length=size(pixels3d,1);
-    vector2d=zeros(length,5);
+   
     new_img=NaN(h,w,3);
-    for i=1:length
+    for i=1:size(pixels3d,1)
         % Estimate X 2d coordinate
-        vector2d(i,1)= round(-f/pixels3d_transformed(i,3)*(pixels3d_transformed(i,1)-vpx)+vpx);
+        idx2= int32(round(-f/pixels3d_transformed(i,3)*(pixels3d_transformed(i,1)-vpx)+vpx));
         % Estimate Y 2d coordinate and also invert so match for the new image
-        vector2d(i,2)= -round(-f/pixels3d_transformed(i,3)*(pixels3d_transformed(i,2)-vpy)+vpy)+h+1;
-        vector2d(i,3:5)=pixels3d(i,4:6);
-        
-        idx1=int32(vector2d(i,2));
-        idx2=int32(vector2d(i,1));
+        idx1= int32(-round(-f/pixels3d_transformed(i,3)*(pixels3d_transformed(i,2)-vpy)+vpy)+h+1);
 
         if idx1<=h &&idx2<=w &&idx1>=1 && idx2>=1
       
-            new_img(idx1,idx2,1:3)=vector2d(i,3:5);
+            new_img(idx1,idx2,1:3)=pixels3d(i,4:6);
         
         end
       
