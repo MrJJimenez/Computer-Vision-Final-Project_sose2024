9dae9a2ea82577b9f47cad3f861c1afc69d6f059
IvanaPijetlovic <pijetlovicivana01@gmail.com>
2024-07-09 17:42:52 +0200

    added new filter to fill missing

--- GUI/create_new_img.m
+++ GUI/create_new_img.m
@@ -1,29 +1,43 @@
 function new_img = create_new_img(pixels3d, vpx, vpy, f, R, T, h, w)
+    % Calculate the center of the point cloud
+    pc_center = mean(pixels3d(:, 1:3), 1);
+    offset = -pc_center(1:3);
     
-    pc_center=mean(pixels3d,1);
-    offset=-pc_center(1:3);
-
-    pixels3d_centered=pixels3d(:,1:3)'+offset';
-    pixels3d_transformed=R*pixels3d_centered+T-offset';
+    % Center the point cloud
+    pixels3d_centered = pixels3d(:, 1:3)' + offset';
+    pixels3d_transformed = R * pixels3d_centered + T - offset';
     pixels3d_transformed = pixels3d_transformed';
-
-   
-    new_img=NaN(h,w,3);
-    for i=1:size(pixels3d,1)
-        % Estimate X 2d coordinate
-        idx2= int32(round(-f/pixels3d_transformed(i,3)*(pixels3d_transformed(i,1)-vpx)+vpx));
-        % Estimate Y 2d coordinate and also invert so match for the new image
-        idx1= int32(-round(-f/pixels3d_transformed(i,3)*(pixels3d_transformed(i,2)-vpy)+vpy)+h+1);
-
-        if idx1<=h &&idx2<=w &&idx1>=1 && idx2>=1
-      
-            new_img(idx1,idx2,1:3)=pixels3d(i,4:6);
-        
-        end
-      
+    
+    % Initialize the new image with NaNs
+    new_img = NaN(h, w, 3);
+    
+    % Project the 3D points to 2D
+    X2 = round(-f ./ pixels3d_transformed(:, 3) .* (pixels3d_transformed(:, 1) - vpx) + vpx);
+    Y2 = -round(-f ./ pixels3d_transformed(:, 3) .* (pixels3d_transformed(:, 2) - vpy) + vpy) + h + 1;
+    
+    % Filter valid indices
+    valid_idx = (Y2 <= h) & (X2 <= w) & (Y2 >= 1) & (X2 >= 1);
+    
+    % Assign colors to the new image
+    valid_pixels = pixels3d(valid_idx, 4:6);
+    X2 = X2(valid_idx);
+    Y2 = Y2(valid_idx);
+    
+    % Convert subscripts to linear indices
+    linear_idx = sub2ind([h, w], Y2, X2);
+    
+    % Assign colors to the corresponding pixels in the new image
+    for c = 1:3
+        temp_img = new_img(:, :, c);
+        temp_img(linear_idx) = valid_pixels(:, c);
+        new_img(:, :, c) = temp_img;
     end
-    new_img=new_img(:,:,1:3);
-    new_img=fillmissing(new_img,'movmedian',21);
-    new_img=uint8(new_img);
-
+    
+    % Fill missing values using inpainting
+    for c = 1:3
+        new_img(:,:,c) = inpaint_nans(new_img(:,:,c));
+    end
+    
+    % Convert to uint8
+    new_img = uint8(new_img);
 end
